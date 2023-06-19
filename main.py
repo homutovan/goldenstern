e8cefa044f28912f4781c079f5676a8711689258
Andrey Khomutov <homutovan@live.com>
2023-06-20 00:42:39 +0700

    add welcome

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from aiobot import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types
+
+from secret import API_TOKEN
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welkome(message: types.Message):
+    await message.reply('Привет, я пока ничего не умею!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
